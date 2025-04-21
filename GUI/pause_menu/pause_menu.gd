7fc8cc67fd8b4618a1c43cadf1267aecb3ebb212
Diego Munoz <dean1h1@hotmail.com>
2025-04-21 00:28:33 -0400

    Inventory and pickup funcs

--- GUI/pause_menu/pause_menu.gd
+++ GUI/pause_menu/pause_menu.gd
@@ -1,8 +1,12 @@
 extends CanvasLayer
 
+signal shown
+signal hidden
 
-@onready var button_save: Button = $VBoxContainer/Button_save
-@onready var button_load: Button = $VBoxContainer/Button_load
+@onready var button_save: Button = $Control/VBoxContainer/Button_save
+@onready var button_load: Button = $Control/VBoxContainer/Button_load
+@onready var item_description: Label = $Control/item_description
+
 
 var is_paused : bool = false
 
@@ -26,13 +30,14 @@ func show_pause_menu() -> void:
 	get_tree().paused = true
 	visible = true
 	is_paused = true
-	button_save.grab_focus()
+	shown.emit()
 
 
 func hide_pause_menu() -> void:
 	get_tree().paused = false
 	visible = false
 	is_paused = false
+	hidden.emit()
 
 func _on_save_pressed() ->void:
 	if is_paused == false:
@@ -50,3 +55,6 @@ func _on_load_pressed() -> void:
 	await LevelManager.level_load_started
 	hide_pause_menu()
 	pass
+
+func update_item_description(new_text : String) -> void:
+	item_description.text = new_text
